bdd7b415043661a7931837b4d0cf061f15496228
Genbu Hase <hasegenbu@gmail.com>
2018-06-24 18:04:45 +0900

    [Merge] Squashed feature/tagTimelinePlus * Fixed a problem that unlisted toot is delivered to FTL & LTL

--- app/services/fan_out_on_write_service.rb
+++ app/services/fan_out_on_write_service.rb
@@ -24,7 +24,7 @@ class FanOutOnWriteService < BaseService
 
     deliver_to_hashtags(status)
 
-    return if status.reply? && status.in_reply_to_account_id != status.account_id
+    return if !status.public_visibility? || status.reply? && status.in_reply_to_account_id != status.account_id
 
     deliver_to_public(status)
     deliver_to_media(status) if status.media_attachments.any?
