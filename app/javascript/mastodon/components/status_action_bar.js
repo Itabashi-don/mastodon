86420563d931301d810f9d81242f48a60bd44b44
--get <hasegenbu@gmail.com>
2019-02-02 09:14:12 +0900

    Implement a new privacy, "Unleakable", instead of "Limited" * Improvement of views for Unleakable   * Add an icon for Unleakable   * Add a warning for Unleakable   * Add localizations for Unleakable
    * Implement delivering for Unleakable
      * Define a new privacy, Unleakable
      * Make sure Unleakable not to be delivered to other instances
      * Change bahavior not to count Unleakable
      * Make sure users not to reblog Unleakable
    
    * Add tasks for fixing conflict between Limited and Unleakable

--- app/javascript/mastodon/components/status_action_bar.js
+++ app/javascript/mastodon/components/status_action_bar.js
@@ -193,6 +193,8 @@ class StatusActionBar extends ImmutablePureComponent {
 
     if (status.get('visibility') === 'direct') {
       reblogIcon = 'envelope';
+    } else if (status.get('visibility') === 'unleakable') {
+      reblogIcon = 'low-vision';
     } else if (status.get('visibility') === 'private') {
       reblogIcon = 'lock';
     }
--- app/models/status.rb
+++ app/models/status.rb
@@ -37,7 +37,7 @@ class Status < ApplicationRecord
 
   update_index('statuses#status', :proper) if Chewy.enabled?
 
-  enum visibility: [:public, :unlisted, :private, :direct, :limited], _suffix: :visibility
+  enum visibility: [:public, :unlisted, :private, :direct, :limited, :unleakable], _suffix: :visibility
 
   belongs_to :application, class_name: 'Doorkeeper::Application', optional: true
 
@@ -189,7 +189,7 @@ class Status < ApplicationRecord
   end
 
   def hidden?
-    private_visibility? || direct_visibility? || limited_visibility?
+    private_visibility? || direct_visibility? || limited_visibility? || unleakable_visibility?
   end
 
   def distributable?
@@ -260,7 +260,7 @@ class Status < ApplicationRecord
     end
 
     def as_home_timeline(account)
-      where(account: [account] + account.following).where(visibility: [:public, :unlisted, :private])
+      where(account: [account] + account.following).where(visibility: [:public, :unlisted, :private, :unleakable])
     end
 
     def as_direct_timeline(account, limit = 20, max_id = nil, since_id = nil, cache_ids = false)
@@ -369,6 +369,8 @@ class Status < ApplicationRecord
         # followers can see followers-only stuff, but also things they are mentioned in.
         # non-followers can see everything that isn't private/direct, but can see stuff they are mentioned in.
         visibility.push(:private) if account.following?(target_account)
+        # Local users followed by author can see unleakable toots.
+        visibility.push(:unleakable) if account.followed_by?(target_account)
 
         scope = left_outer_joins(:reblog)
 
@@ -475,7 +477,7 @@ class Status < ApplicationRecord
   end
 
   def increment_counter_caches
-    return if direct_visibility?
+    return if direct_visibility? || unleakable_visibility?
 
     account&.increment_count!(:statuses_count)
     reblog&.increment_count!(:reblogs_count) if reblog? && (public_visibility? || unlisted_visibility?)
@@ -483,7 +485,7 @@ class Status < ApplicationRecord
   end
 
   def decrement_counter_caches
-    return if direct_visibility? || marked_for_mass_destruction?
+    return if direct_visibility? || unleakable_visibility? || marked_for_mass_destruction?
 
     account&.decrement_count!(:statuses_count)
     reblog&.decrement_count!(:reblogs_count) if reblog? && (public_visibility? || unlisted_visibility?)
